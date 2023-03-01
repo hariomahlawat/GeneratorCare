1a1f1bb67451ec641bf6c1f6b19c8a8975687cdc
Hari Om Ahlawat <hariomahlawat@gmail.com>
2023-03-01 13:55:36 +0530

    Setting up App Routing and enum class

--- app/src/main/java/com/hariomahlawat/generatorcare/navigation/GeneratorCareScreens.kt
+++ app/src/main/java/com/hariomahlawat/generatorcare/navigation/GeneratorCareScreens.kt
@@ -1,4 +1,26 @@
 package com.hariomahlawat.generatorcare.navigation
 
 enum class GeneratorCareScreens {
+    SplashScreen,
+    HomeScreen,
+    AddGeneratorScreen,
+    AdvisoryScreen,
+    MaintenanceAlertsScreen,
+    GeneratorsListScreen,
+    MaintenanceRecordScreen;
+
+    companion object {
+        fun fromRoute(route: String?): GeneratorCareScreens
+        = when (route?.substringBefore("/")) {
+            SplashScreen.name -> SplashScreen
+            HomeScreen.name -> HomeScreen
+            AddGeneratorScreen.name -> AddGeneratorScreen
+            AdvisoryScreen.name -> AdvisoryScreen
+            MaintenanceAlertsScreen.name -> MaintenanceAlertsScreen
+            GeneratorsListScreen.name -> GeneratorsListScreen
+            MaintenanceRecordScreen.name -> MaintenanceRecordScreen
+            null -> HomeScreen
+            else -> throw IllegalArgumentException("Route $route is not recognized")
+        }
+    }
 }
