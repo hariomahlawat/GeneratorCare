3300ac688ae7935a206f384e620a70fcf28fb549
Hari Om Ahlawat <hariomahlawat@gmail.com>
2023-03-03 10:24:08 +0530

    @TypeConverters added

--- app/src/main/java/com/hariomahlawat/generatorcare/data/GeneratorCareDatabase.kt
+++ app/src/main/java/com/hariomahlawat/generatorcare/data/GeneratorCareDatabase.kt
@@ -2,9 +2,13 @@ package com.hariomahlawat.generatorcare.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.hariomahlawat.generatorcare.model.Generator
+import com.hariomahlawat.generatorcare.utils.DateConverter
+import com.hariomahlawat.generatorcare.utils.UUIDConverter
 
 @Database(entities = [Generator::class], version = 1, exportSchema = false)
+@TypeConverters(DateConverter::class, UUIDConverter::class)
 abstract class GeneratorCareDatabase : RoomDatabase() {
     abstract fun generatorDao() : GeneratorDatabseDao
 }
