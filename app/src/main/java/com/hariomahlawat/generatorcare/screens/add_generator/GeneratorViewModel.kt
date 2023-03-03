8ec280429a7784b7521371750c6053289f444193
Hari Om Ahlawat <hariomahlawat@gmail.com>
2023-03-03 12:20:38 +0530

    init

--- app/src/main/java/com/hariomahlawat/generatorcare/screens/add_generator/GeneratorViewModel.kt
+++ app/src/main/java/com/hariomahlawat/generatorcare/screens/add_generator/GeneratorViewModel.kt
@@ -1,5 +1,6 @@
 package com.hariomahlawat.generatorcare.screens.add_generator
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -16,6 +17,20 @@ import javax.inject.Inject
 class GeneratorViewModel  @Inject constructor(private val repository: GeneratorRepository):ViewModel(){
     private val _generatorList = MutableStateFlow<List<Generator>>(emptyList())
     val generatorList = _generatorList.asStateFlow()
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.getAllGenerators().distinctUntilChanged()
+                .collect { listOfGenerators ->
+                    if (listOfGenerators.isNullOrEmpty()) {
+                        Log.d("Empty", ": Empty list")
+                    } else {
+                        _generatorList.value = listOfGenerators
+                    }
+
+                }
+        }
+        }
 
     fun addGenerator(generator: Generator) = viewModelScope.launch { repository.addGenerator(generator) }
     fun removeGenerator(generator: Generator) = viewModelScope.launch { repository.deleteGenerator(generator) }
