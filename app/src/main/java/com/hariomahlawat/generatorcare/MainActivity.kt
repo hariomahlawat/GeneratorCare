ac306b2fb716f8eec7fe1694eb333cf8f660ab57
Hari Om Ahlawat <hariomahlawat@gmail.com>
2023-03-04 13:25:06 +0530

    App Drawer done. Navigation to Home Screen done.

--- app/src/main/java/com/hariomahlawat/generatorcare/MainActivity.kt
+++ app/src/main/java/com/hariomahlawat/generatorcare/MainActivity.kt
@@ -1,23 +1,37 @@
 package com.hariomahlawat.generatorcare
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.hariomahlawat.generatorcare.navigation.GeneratorCareNavigation
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.hariomahlawat.generatorcare.navigation.*
 import com.hariomahlawat.generatorcare.ui.theme.GeneratorCareTheme
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -25,7 +39,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GeneratorCareTheme {
+                //GeneratorCareApp()
+
                 GeneratorCareApp()
+
             }
         }
     }
--- app/src/main/java/com/hariomahlawat/generatorcare/screens/home/HomeScreen.kt
+++ app/src/main/java/com/hariomahlawat/generatorcare/screens/home/HomeScreen.kt
@@ -1,20 +1,94 @@
 package com.hariomahlawat.generatorcare.screens.home
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.hariomahlawat.generatorcare.GeneratorCareApp
 import com.hariomahlawat.generatorcare.components.AppButton
-import com.hariomahlawat.generatorcare.navigation.GeneratorCareScreens
+import com.hariomahlawat.generatorcare.navigation.*
+import kotlinx.coroutines.launch
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun HomeScreen(navController: NavController){
+fun HomeScreen(navController: NavController) {
 
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            AppBar(
+                onNavigationIconClick = {
+                    scope.launch {
+                        scaffoldState.drawerState.open()
+                    }
+                }
+            )
+        },
+        drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
+        drawerContent = {
+            DrawerHeader()
+            DrawerBody(
+                items = listOf(
+                    MenuItem(
+                        id = "home",
+                        title = "Home",
+                        contentDescription = "Go to home screen",
+                        icon = Icons.Default.Home,
+                        navigation_address = GeneratorCareScreens.HomeScreen.name
+
+                    ),
+                    MenuItem(
+                        id = "maintenance_Record",
+                        title = "Maintenance History",
+                        contentDescription = "Get Maintenance History",
+                        icon = Icons.Default.List,
+                        navigation_address = GeneratorCareScreens.MaintenanceRecordScreen.name
+                    ),
+                    MenuItem(
+                        id = "advisory",
+                        title = "Adviisory",
+                        contentDescription = "Go to advisory screen",
+                        icon = Icons.Default.Info,
+                        navigation_address = GeneratorCareScreens.AddGeneratorScreen.name
+                    ),
+
+                    ),
+                onItemClick = {
+                    navController.navigate(route = it.navigation_address)
+                }
+            )
+        }
+    ) {
+        HomeScreenInner(navController = navController)
+    }
+
+}
+@Composable
+fun HomeScreenInner(navController:NavController){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize(),
+    ) {
         Text(text = "Home Screen")
         AppButton(text = "Add New Generator", onClick = {
             navController.navigate(GeneratorCareScreens.AddGeneratorScreen.name)
         })
     }
 }
+
+
