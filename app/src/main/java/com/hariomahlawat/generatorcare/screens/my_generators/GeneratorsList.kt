efbb44639aea327c235020dd91b8ba15d68fbccf
Hari Om Ahlawat <hariomahlawat@gmail.com>
2023-03-04 21:06:34 +0530

    App Drawer added.

--- app/src/main/java/com/hariomahlawat/generatorcare/screens/my_generators/GeneratorsList.kt
+++ app/src/main/java/com/hariomahlawat/generatorcare/screens/my_generators/GeneratorsList.kt
@@ -1,9 +1,51 @@
 package com.hariomahlawat.generatorcare.screens.my_generators
 
+import android.annotation.SuppressLint
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavController
+import com.hariomahlawat.generatorcare.navigation.AppBar
+import com.hariomahlawat.generatorcare.navigation.DrawerBody
+import com.hariomahlawat.generatorcare.navigation.DrawerHeader
+import com.hariomahlawat.generatorcare.navigation.MenuItemData
+import com.hariomahlawat.generatorcare.screens.maintenance_record.MaintenanceRecordScreenInner
+import kotlinx.coroutines.launch
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun GeneratorsListScreen(navController: NavController){
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            AppBar(
+                title="Maintenance Records",
+                onNavigationIconClick = {
+                    scope.launch {
+                        scaffoldState.drawerState.open()
+                    }
+                }
+            )
+        },
+        drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
+        drawerContent = {
+            DrawerHeader()
+            DrawerBody(
+                items = MenuItemData().loadMenuItems(),
+                onItemClick = {
+                    navController.navigate(route = it.navigation_address)
+                }
+            )
+        }
+    ) {
+        GeneratorsListScreenInner(navController=navController)
+    }
+}
+
+@Composable
+fun GeneratorsListScreenInner(navController: NavController){
 
 }
