0f2cb79e1033e1ba982a7fb6e47e6e1b5bf6db69
Hari Om Ahlawat <hariomahlawat@gmail.com>
2023-03-05 11:31:00 +0530

    Datepicker configured.

--- app/src/main/java/com/hariomahlawat/generatorcare/screens/add_generator/AddGenerator.kt
+++ app/src/main/java/com/hariomahlawat/generatorcare/screens/add_generator/AddGenerator.kt
@@ -1,10 +1,9 @@
 package com.hariomahlawat.generatorcare.screens.add_generator
 
 import android.annotation.SuppressLint
+import android.widget.Toast
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -35,9 +34,15 @@ import com.hariomahlawat.generatorcare.model.Generator
 import com.hariomahlawat.generatorcare.navigation.*
 import com.hariomahlawat.generatorcare.screens.home.HomeScreenInner
 import com.hariomahlawat.generatorcare.utils.formatDate
+import com.vanpra.composematerialdialogs.MaterialDialog
+import com.vanpra.composematerialdialogs.datetime.date.datepicker
+import com.vanpra.composematerialdialogs.rememberMaterialDialogState
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -83,14 +88,19 @@ fun AddGeneratorScreen(navController: NavController){
 fun AddGenerator(generators: List<Generator>,
                  onAddGenerator: (Generator) -> Unit
 ){
-    var registration_number by remember {
-        mutableStateOf("")
-    }
-    var make by remember {
-        mutableStateOf("")
-    }
-    var model by remember {
-        mutableStateOf("")
+    var registration_number by remember {mutableStateOf("")}
+    var make by remember {mutableStateOf("")}
+    var model by remember {mutableStateOf("")}
+    var hours_run by remember {mutableStateOf("")}
+    var kva_rating by remember {mutableStateOf("")}
+    var issueDate by remember {mutableStateOf(LocalDate.now())}
+    val dateDialogState = rememberMaterialDialogState()
+    val formattedDate by remember {
+        derivedStateOf {
+            DateTimeFormatter
+                .ofPattern("dd MMM yyyy")
+                .format(issueDate)
+        }
     }
     val context = LocalContext.current
 
@@ -126,24 +136,86 @@ fun AddGenerator(generators: List<Generator>,
             onTextChange = {
                 model = it
             })
+        Divider(thickness = 1.dp)
+        //-------------- date picker----------------------------------
+        Row(horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)) {
+            Button(onClick = {
+                dateDialogState.show()
+            }) {
+                Text(text = "Pick Issue date")
+            }
+            Spacer(modifier = Modifier.padding(16.dp))
+            Text(text = formattedDate)
+
+        }
+        Divider(thickness = 1.dp, modifier = Modifier.padding(5.dp))
+
+        MaterialDialog(
+            dialogState = dateDialogState,
+            buttons = {
+                positiveButton(text = "Ok")
+                negativeButton(text = "Cancel")
+            }
+        ) {
+            this.datepicker(
+                initialDate = LocalDate.now(),
+                title = "Pick a date",
+            ){
+                issueDate=it
+            }
+        }
+
+        //--------------------------------------------------------
+        AppInputText(
+            modifier = Modifier.padding(
+                top = 9.dp,
+                bottom = 8.dp),
+            text = hours_run,
+            label = "Hours Run",
+            onTextChange = {
+                hours_run = it
+            })
+        AppInputText(
+            modifier = Modifier.padding(
+                top = 9.dp,
+                bottom = 8.dp),
+            text = kva_rating,
+            label = "KVA Rating",
+            onTextChange = {
+                kva_rating = it
+            })
 
         AppButton(text = "Save",
             onClick = {
-                if (registration_number.isNotEmpty() && make.isNotEmpty()) {
+                if (registration_number.isNotEmpty()
+                    && make.isNotEmpty()
+                    && model.isNotEmpty()
+                    && hours_run.isNotEmpty()
+                    && kva_rating.isNotEmpty()
+                    && issueDate != LocalDate.now()
+                ) {
                     // add to database
                     onAddGenerator(
                         Generator(
                             registration_number=registration_number,
                             make = make,
                             model = model,
-                            kva_rating = "",
-                            hours_run = 1,
-                            issueDate = Date.from(Instant.now())
+                            kva_rating = kva_rating,
+                            hours_run = hours_run.toInt(),
+                            issueDate = Date.from(issueDate.atStartOfDay(ZoneId.systemDefault()).toInstant())
                         )
                     )
 
                     registration_number = ""
                     make = ""
+                    model=""
+                    hours_run=""
+                    kva_rating=""
+                    issueDate=LocalDate.now()
                     showToast(context,"Generator added.")
                 }
                 else{
@@ -152,35 +224,10 @@ fun AddGenerator(generators: List<Generator>,
             })
 
 
-        Divider(modifier = Modifier.padding(10.dp))
-
 
 
 
     }
 }
 
-@Composable
-fun GeneratorRow(
-    modifier: Modifier = Modifier,
-    generator: Generator,
-    onGeneratorClicked: (Generator) -> Unit) {
-    Surface(
-        modifier
-            .padding(4.dp)
-            .clip(RoundedCornerShape(topEnd = 33.dp, bottomStart = 33.dp))
-            .fillMaxWidth(),
-        color = Color(0xFFDFE6EB),
-        elevation = 6.dp) {
-        Column(modifier
-            .clickable {onGeneratorClicked(generator) }
-            .padding(horizontal = 14.dp, vertical = 6.dp),
-            horizontalAlignment = Alignment.Start) {
-            Text(text = generator.registration_number,
-                style = MaterialTheme.typography.subtitle2)
-            Text(text = generator.make, style = MaterialTheme.typography.subtitle1)
-            Text(text = formatDate(generator.entryDate.time),
-                style = MaterialTheme.typography.caption)
-        }
-    }
-}
+
--- app/src/main/java/com/hariomahlawat/generatorcare/screens/my_generators/GeneratorsList.kt
+++ app/src/main/java/com/hariomahlawat/generatorcare/screens/my_generators/GeneratorsList.kt
@@ -1,13 +1,22 @@
 package com.hariomahlawat.generatorcare.screens.my_generators
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Scaffold
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.hariomahlawat.generatorcare.model.Generator
@@ -15,9 +24,9 @@ import com.hariomahlawat.generatorcare.navigation.AppBar
 import com.hariomahlawat.generatorcare.navigation.DrawerBody
 import com.hariomahlawat.generatorcare.navigation.DrawerHeader
 import com.hariomahlawat.generatorcare.navigation.MenuItemData
-import com.hariomahlawat.generatorcare.screens.add_generator.GeneratorRow
 import com.hariomahlawat.generatorcare.screens.add_generator.GeneratorViewModel
 import com.hariomahlawat.generatorcare.screens.maintenance_record.MaintenanceRecordScreenInner
+import com.hariomahlawat.generatorcare.utils.formatDate
 import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -66,6 +75,35 @@ fun GeneratorsListScreenInner(navController: NavController,
         items(generators){generator ->
             GeneratorRow(generator = generator,
                 onGeneratorClicked = {onRemoveGenerator(it)})
+        }
+    }
+}
+
+@Composable
+fun GeneratorRow(
+    modifier: Modifier = Modifier,
+    generator: Generator,
+    onGeneratorClicked: (Generator) -> Unit) {
+    Surface(
+        modifier
+            .padding(4.dp)
+            .clip(RoundedCornerShape(topEnd = 13.dp, bottomStart = 13.dp))
+            .fillMaxWidth(),
+        color = Color(0xFFDFE6EB),
+        elevation = 6.dp) {
+        Column(modifier
+            .clickable {onGeneratorClicked(generator) }
+            .padding(horizontal = 14.dp, vertical = 6.dp),
+            horizontalAlignment = Alignment.Start) {
+            Text(text = "Regd No: " +generator.registration_number, style = MaterialTheme.typography.subtitle2)
+            Text(text = "Make: " +generator.make, style = MaterialTheme.typography.subtitle1)
+            Text(text = "Model: " +generator.model, style = MaterialTheme.typography.subtitle1)
+            Text(text = "KVA Rating: " +generator.kva_rating, style = MaterialTheme.typography.subtitle1)
+            Text(text = "Hours run: " +generator.hours_run.toString(), style = MaterialTheme.typography.subtitle1)
+            Text(text = "Issue Date: " + generator.issueDate?.let { formatDate(it.time) },
+                style = MaterialTheme.typography.subtitle1)
+            Text(text = "Data entry Date: " + formatDate(generator.entryDate.time),
+                style = MaterialTheme.typography.caption)
         }
     }
 }
